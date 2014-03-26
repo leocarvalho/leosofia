0d586f363de92df9c8f8975fe618ffa4897d3919
Leonardo Carvalho <lcarvalho@cos.ufrj.br>
2014-03-26 19:39:11 -0300

    Separating javascripts from html.

---
+++ functions.js
@@ -0,0 +1,7 @@
+// Function to randomly change the main image.
+function displayImage(){
+	var num = Math.floor(Math.random()*22)
+	document.getElementById("buddy_image").src = 'imgs/out-'+num.toString()+'.png'
+}
+
+displayImage();
